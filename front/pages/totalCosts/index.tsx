caeeffce89a4eb1e2c00970400535218d30739c8
ktodkf <62442562+ktodkf@users.noreply.github.com>
2022-12-20 16:22:07 +0300

    Add files via upload

---
+++ front/pages/totalCosts/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import TypeTotalCostsPage from '../../src/components/TypeTotalCostsPage'
+
+export default function CostsRoute() {
+	return (
+		<TypeTotalCostsPage/>
+	)
+}
