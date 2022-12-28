d3ca917f0b7ac010058e591311a1f6fa8bc2b787
anatolii.popov <anatolii.popov@travellline.ru>
2022-12-28 22:54:35 +0300

    рефакторинг

--- back/FurnitureHelper/src/ExtranetAPI/Controllers/AnalyticsController.cs
+++ back/FurnitureHelper/src/ExtranetAPI/Controllers/AnalyticsController.cs
@@ -8,33 +8,23 @@ using Swashbuckle.AspNetCore.Annotations;
 
 namespace ExtranetAPI.Controllers;
 
-// как считать вхождение в период?
-
-//доделать k1 k2 по периоду и по Id проекта
-
-// все отдельно для projectId нужно
-// отсальное по периоду
-
 [Route( "analytics" )]
 public class AnalyticsController : ControllerBase
 {
     private readonly IProjectRepository _projectRepository;
     private readonly IProjectBudgetRepository _projectBudgetRepository;
     private readonly IAnalyticsService _analyticsService;
-    private readonly IBuisnessCostRepository _buisnessCostRepository;
     private readonly ICostRepository _costRepository;
 
     public AnalyticsController(
         IProjectRepository projectRepository,
         IProjectBudgetRepository projectBudgetRepository,
         IAnalyticsService analyticsService,
-        IBuisnessCostRepository buisnessCostRepository,
         ICostRepository costRepository )
     {
         _projectRepository = projectRepository;
         _projectBudgetRepository = projectBudgetRepository;
         _analyticsService = analyticsService;
-        _buisnessCostRepository = buisnessCostRepository;
         _costRepository = costRepository;
     }
     
@@ -44,7 +34,7 @@ public class AnalyticsController : ControllerBase
     /// <param name="projectId"></param>
     /// <returns></returns>
     [HttpGet( "margin/{projectId}" )]
-    [SwaggerResponse( statusCode: 200, type: typeof( decimal ), description: "Маржа по проекту" )]//done
+    [SwaggerResponse( statusCode: 200, type: typeof( decimal ), description: "Маржа по проекту" )]
     public async Task<IActionResult> GetProjectMargin( [FromRoute] int projectId )
     {
         ProjectBudget projectBudget = await _projectBudgetRepository.GetByProjectId( projectId );
@@ -61,7 +51,7 @@ public class AnalyticsController : ControllerBase
     /// <param name="searchAnalytic"></param>
     /// <returns></returns>
     [HttpPost( "margin" )]
-    [SwaggerResponse( statusCode: 200, type: typeof( ProjectsMagrinDto ), description: "Маржа по проектам за период" )]//done
+    [SwaggerResponse( statusCode: 200, type: typeof( ProjectsMagrinDto ), description: "Маржа по проектам за период" )]
     public async Task<IActionResult> GetProjectMargin( [FromBody] SearchAnalyticDto searchAnalytic )
     {
         IReadOnlyList<Project> projects = await _projectRepository.GetAll( searchAnalytic.Period.StartDate.Date, searchAnalytic.Period.EndDate.Date );
@@ -113,7 +103,7 @@ public class AnalyticsController : ControllerBase
     /// <param name="projectId"></param>
     /// <returns></returns>
     [HttpGet("profitNorm/{projectId}")]
-    [SwaggerResponse(statusCode: 200, type: typeof(string), description: "Норма прибыли")] //done
+    [SwaggerResponse(statusCode: 200, type: typeof(string), description: "Норма прибыли")]
     public async Task<IActionResult> GetProfitNorm([FromRoute] int projectId)
     {
         ProjectBudget projectBudget = await _projectBudgetRepository.GetByProjectId(projectId);
@@ -124,38 +114,13 @@ public class AnalyticsController : ControllerBase
                 projectBudget.ProjectCost ) );
     }
 
-    /*/// <summary>
-    /// Норма прибыли по проектам
-    /// </summary>
-    /// <param name="searchAnalytic"></param>
-    /// <returns></returns>
-    [HttpPost( "profitNorm" )]
-    [SwaggerResponse( statusCode: 200, type: typeof( string ), description: "Норма прибыли по проектам" )]//откат
-    public async Task<IActionResult> GetProfitNorm( [FromBody] SearchAnalyticDto searchAnalytic )
-    {
-        IReadOnlyList<Project> projects = await _projectRepository
-            .GetAll( searchAnalytic.Period.StartDate, searchAnalytic.Period.EndDate );
-        
-        if ( !String.IsNullOrEmpty( searchAnalytic.Name ) )
-        {
-            projects = projects.Where( x => x.Name == searchAnalytic.Name ).ToList();
-        }
-
-        IReadOnlyList<ProjectBudget> projectBudgets = await _projectBudgetRepository
-            .GetByProjectIds( projects.Select( x => x.Id ).ToList() );
-        
-        //dto
-
-        return Ok();
-    }*/
-    
     /// <summary>
     /// Норма прибавочной стоимости
     /// </summary>
     /// <param name="projectId"></param>
     /// <returns></returns>
     [HttpGet( "rateOfSurplusValue/{projectId}" )]
-    [SwaggerResponse( statusCode: 200, type: typeof( string ), description: "Норма прибавочной стоимости" )]//done
+    [SwaggerResponse( statusCode: 200, type: typeof( string ), description: "Норма прибавочной стоимости" )]
     public async Task<IActionResult> GetRateOfSurplusValue( [FromRoute] int projectId )
     {
         ProjectBudget projectBudget = await _projectBudgetRepository.GetByProjectId( projectId );
@@ -165,39 +130,14 @@ public class AnalyticsController : ControllerBase
                 projectBudget.CostPayments.Select( x => x.Amount ).Sum(),
                 projectBudget.ProjectCost ) );
     }
-    
-    /*/// <summary>
-    /// Норма прибавочной стоимости по проектам
-    /// </summary>
-    /// <param name="searchAnalytic"></param>
-    /// <returns></returns>
-    [HttpPost( "rateOfSurplusValue" )]
-    [SwaggerResponse( statusCode: 200, type: typeof( decimal ), description: "Норма прибавочной стоимости по проектам" )]//откат
-    public async Task<IActionResult> GetRateOfSurplusValue( [FromBody] SearchAnalyticDto searchAnalytic )
-    {
-        IReadOnlyList<Project> projects = await _projectRepository
-            .GetAll( searchAnalytic.Period.StartDate, searchAnalytic.Period.EndDate );
-        
-        if ( !String.IsNullOrEmpty( searchAnalytic.Name ) )
-        {
-            projects = projects.Where( x => x.Name == searchAnalytic.Name ).ToList();
-        }
 
-        IReadOnlyList<ProjectBudget> projectBudgets = await _projectBudgetRepository
-            .GetByProjectIds( projects.Select( x => x.Id ).ToList() );
-
-        //dto
-        
-        return Ok();
-    }*/
-    
     /// <summary>
     /// Просроченные проекты
     /// </summary>
     /// <param name="searchAnalytic"></param>
     /// <returns></returns>
     [HttpPost( "outDatedProjects" )]
-    [SwaggerResponse( statusCode: 200, type: typeof( OutdatedProjectsDto ), description: "Просроченные проекты" )]//done
+    [SwaggerResponse( statusCode: 200, type: typeof( OutdatedProjectsDto ), description: "Просроченные проекты" )]
     public async Task<IActionResult> GetOutdatedProjects( [FromBody] SearchAnalyticDto searchAnalytic )
     {
         DateTime currentDate = DateTime.Now;
@@ -245,7 +185,7 @@ public class AnalyticsController : ControllerBase
     /// <param name="projectId"></param>
     /// <returns></returns>
     [HttpGet( "spendingOnCosts/{projectId}" )]
-    [SwaggerResponse( statusCode: 200, type: typeof( SpendingOnProjectCostsDto ), description: "Траты на издержки по проекту" )]//done
+    [SwaggerResponse( statusCode: 200, type: typeof( SpendingOnProjectCostsDto ), description: "Траты на издержки по проекту" )]
     public async Task<IActionResult> GetSpendingOnProjectCosts( [FromRoute] int projectId )
     {
         Project project = await _projectRepository.GetById( projectId );
@@ -280,7 +220,7 @@ public class AnalyticsController : ControllerBase
     /// <param name="searchAnalytic"></param>
     /// <returns></returns>
     [HttpPost( "spendingOnCosts" )]
-    [SwaggerResponse( statusCode: 200, type: typeof( List<SpendingOnCostDto> ), description: "Траты на издержки" )]//done
+    [SwaggerResponse( statusCode: 200, type: typeof( List<SpendingOnCostDto> ), description: "Траты на издержки" )]
     public async Task<IActionResult> GetSpendingCosts( [FromBody] SearchAnalyticDto searchAnalytic )
     {
         List<Cost> costs = await _costRepository.GetAll();
@@ -324,7 +264,7 @@ public class AnalyticsController : ControllerBase
     /// <param name="period"></param>
     /// <returns></returns>
     [HttpPost( "projectsPrices" )]
-    [SwaggerResponse( statusCode: 200, type: typeof( ProjectsPricesDto ), description: "Цены проектов" )] //group by name
+    [SwaggerResponse( statusCode: 200, type: typeof( ProjectsPricesDto ), description: "Цены проектов" )]
     public async Task<IActionResult> GetProjectsPrices( [FromBody] Period period )
     {
         IReadOnlyList<Project> projects = await _projectRepository
@@ -344,7 +284,13 @@ public class AnalyticsController : ControllerBase
                 ? new ProjectsPricesDto() 
                 : new ProjectsPricesDto
                     {
-                        ProjectPrices = projectPrices,
+                        ProjectPrices = projectPrices
+                            .GroupBy( x => x.ProjectName )
+                            .Select( pps => new ProjectPriceDto
+                            {
+                                ProjectPrice = pps.Select( pp => pp.ProjectPrice ).Sum(),
+                                ProjectName = pps.Key
+                            } ).ToList(),
                         AveragePrice = decimal.Divide(
                             projectPrices.Select( x => x.ProjectPrice ).Sum(),
                             projectPrices.Count ),
