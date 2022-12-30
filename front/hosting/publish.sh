a04290f3be3de7184a7e12c58ed93f576e8c2189
AndreyIo0I <andrbeckov@gmail.com>
2022-12-30 09:24:34 +0300

    поправил публикацию

--- front/hosting/publish.sh
+++ front/hosting/publish.sh
@@ -1,11 +1,16 @@
-cp FHFront.service /etc/systemd/system/FHFront.service;\
-cd .. ;\
-yarn install;\
-yarn build;\
-systemctl stop FHFront;\
-cp -r .next/static .next/standalone/.next/static;\
-cp -r public .next/standalone/public;\
-cp -r .next/standalone /root/srv/FHFront;\
-systemctl daemon-reload;\
-systemctl start FHFront;\
-systemctl status FHFront;
+systemctl stop FHFront
+
+cp FHFront.service /etc/systemd/system/FHFront.service
+
+cd ..
+
+yarn install
+yarn build
+
+cp -r .next/static .next/standalone/.next/static
+cp -r public .next/standalone/public
+cp -r .next/standalone /root/srv/FHFront
+
+systemctl daemon-reload
+systemctl start FHFront
+systemctl status FHFront
